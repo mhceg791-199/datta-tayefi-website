a36ed5072e1aae5eb1392a92dd81bd22631944cb
mhceg791-199 <mhceg791@gmail.com>
2025-11-20 17:52:01 +0200

    updated

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -7,13 +7,12 @@ import DescriptionAbout from "../../components/shared/DescriptionAbout/Descripti
 function About() {
   const paragraphs = [
     "Datta & Tayefi is a proud subsidiary of Mosaic Holding Corporation — an investment firm built on the belief that when exceptional forces come together, they create something far more powerful than they ever could alone.",
-  ];
-
-  const paragraphs2 = [
     "That belief brought two established names into the Mosaic family. Arup Datta Architects Ltd., known for shaping meaningful, culturally rooted designs across continents.",
     "And Max Tayefi Architect Inc., recognized for its technical strength, precision, and decades of trusted project delivery."
   ];
-  const paragraphs3 = [
+
+  
+  const paragraphs2 = [
     "Within Mosaic’s vision of unity and purpose, their convergence felt less like a decision and more like an evolution — a natural alignment of capability, experience, and ambition.",
 
     "From that alignment, Datta & Tayefi emerged. Not as a simple merge, but as a new powerhouse — strengthened by two proven legacies and guided by Mosaic Holding’s belief that greatness is created when the right pieces come together with intention.",
@@ -30,7 +29,7 @@ function About() {
         secondWord="Story"
         paragraphs={paragraphs}
         paragraphs2={paragraphs2}
-        paragraphs3={paragraphs3}
+        // paragraphs3={paragraphs3}
         particleColors={["#e0e0d4", "#e0e0d4"]}
         height="h-[85vh] md:h-[65vh]"
       />

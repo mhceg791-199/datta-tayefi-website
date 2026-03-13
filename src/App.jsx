8814fd38b8c31640e2f0c0b9938ea60799b224bc
mhceg791-199 <mhceg791@gmail.com>
2026-03-13 09:12:44 +0200

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
-// import "./App.css";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+import "./App.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-// import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-// import Home from "./pages/home/home";
-// import Layout from "./Layout";
-// import About from "./pages/about/about";
-// import Projects from "./pages/projects/projects";
-// import ProjectsDetails from "./pages/projectsDetails/projectsDetails";
-// import LandingPage from "./pages/LandingPage/LandingPage";
-// import CategoryProjects from "./pages/ProjectCategory/ProjectCategory";
-// import ScrollToTopButton from "./components/shared/ScrollToTopButton/ScrollToTopButton";
-// import Services from "./pages/Services/Services";
-// import ProjectsFilter from "./pages/projects/ProjectsFilter";
-// import Career from "./pages/Career/Career";
-// import ContactUs from "./pages/ContactUs/ContactUs";
-// import Terms from "./pages/Terms/Terms";
-// import Privacy from "./pages/Privacy/Privacy";
+import Home from "./pages/home/home";
+import Layout from "./Layout";
+import About from "./pages/about/about";
+import Projects from "./pages/projects/projects";
+import ProjectsDetails from "./pages/projectsDetails/projectsDetails";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import CategoryProjects from "./pages/ProjectCategory/ProjectCategory";
+import ScrollToTopButton from "./components/shared/ScrollToTopButton/ScrollToTopButton";
+import Services from "./pages/Services/Services";
+import ProjectsFilter from "./pages/projects/ProjectsFilter";
+import Career from "./pages/Career/Career";
+import ContactUs from "./pages/ContactUs/ContactUs";
+import Terms from "./pages/Terms/Terms";
+import Privacy from "./pages/Privacy/Privacy";
 
 function App() {
   return (
     <>
-      {/* <Routes> */}
+      <Routes>
         {/* LandingPage */}
-        {/* <Route path="/" element={<LandingPage />} /> */}
+        <Route path="/" element={<LandingPage />} />
 
-        {/* <Route
+        <Route
           path="/*"
           element={
             <Layout>
@@ -50,8 +50,8 @@ function App() {
           }
         />
       </Routes>
-      <ScrollToTopButton /> */}
-      <div
+      <ScrollToTopButton />
+      {/* <div
       style={{
         height: "100vh",
         display: "flex",
@@ -60,8 +60,7 @@ function App() {
         backgroundColor: "#ffffff",
       }}
     >
-      {/* صفحة بيضاء فاضية */}
-    </div>
+    </div> */}
     </>
   );
 }
